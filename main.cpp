79570c2c7452ff5c8091ae9d9e795f2e295d95fe
Zaid Khan <zkaff321@gmail.com>
2026-04-16 14:55:37 +0500

    new fiiles added

--- main.cpp
+++ main.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main(){
     cout<<"ffff";
+    cout<<"\n hi";
     return 0;
 }
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ def watch(name):
    return f"heelo {name}"
 
 print(watch("ali"))
+print("helo heloo")
